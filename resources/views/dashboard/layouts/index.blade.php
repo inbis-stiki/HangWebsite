9597c5d0f1386550534319253a248ab23b7639c5
Rizal Abdul Basith <rizalgu4rd14n@gmail.com>
2022-06-08 01:35:52 +0700

    menambahkan layout pada dashboard dan master lokasi

---
+++ resources/views/dashboard/layouts/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" type="text/css" media="all">
+</head>
+
+<body class="bg-gray-100">
+    <div class="grid grid-cols-9">
+        @include('dashboard.layouts.sidebar')
+        <main class="md:col-span-7 col-span-9">
+            @include('dashboard.layouts.navbar')
+            <div class="bg-gray-100 min-h-screen py-8">
+                <div class="px-12 mx-auto">
+                    @yield('content')
+                </div>
+            </div>
+        </main>
+    </div>
+    @yield('footer')
+</body>
+
+</html>
---
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -0,0 +1,59 @@
+    <aside class="self-start sticky top-0 col-span-2 px-5 py-9 border-r border-gray-200 hidden md:block bg-white">
+        <img src="{{asset('img/finna.png')}}" class="w-32 mx-auto" alt="">
+        <div class="mt-6 space-y-4">
+            <div>
+                <a href="#" class="flex flex-row rounded-lg px-4 py-3 bg-orange-100 space-x-7">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 22" class="w-6 h-6 fill-orange-500">
+                        <path d="m18.667 6.667-6.5-5.699a3.25 3.25 0 0 0-4.334 0l-6.5 5.699A3.25 3.25 0 0 0 .25 9.115v9.468a3.25 3.25 0 0 0 3.25 3.25h13a3.25 3.25 0 0 0 3.25-3.25V9.104a3.25 3.25 0 0 0-1.083-2.437Zm-6.5 13H7.833V14.25a1.083 1.083 0 0 1 1.084-1.083h2.166a1.083 1.083 0 0 1 1.084 1.083v5.417Zm5.416-1.084a1.083 1.083 0 0 1-1.083 1.084h-2.167V14.25a3.25 3.25 0 0 0-3.25-3.25H8.917a3.25 3.25 0 0 0-3.25 3.25v5.417H3.5a1.084 1.084 0 0 1-1.083-1.084V9.104a1.083 1.083 0 0 1 .368-.812l6.5-5.688a1.083 1.083 0 0 1 1.43 0l6.5 5.688a1.083 1.083 0 0 1 .368.812v9.48Z" />
+                    </svg>
+                    <span class="font-semibold primary-orange">Dashboard</span>
+                </a>
+            </div>
+            <div class="space-y-4 overflow-y">
+                <a href="#" class="flex flex-row rounded-lg px-4 py-3 space-x-7">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 26" class="w-6 h-6 fill-gray-300">
+                        <path d="M17.333 21.667H8.667a3.25 3.25 0 0 1-3.25-3.25V7.583a1.083 1.083 0 0 0-2.167 0v10.834a5.417 5.417 0 0 0 5.417 5.416h8.666a1.083 1.083 0 1 0 0-2.166Zm-6.5-7.584a1.083 1.083 0 0 0 1.084 1.084h5.416a1.083 1.083 0 1 0 0-2.167h-5.416a1.083 1.083 0 0 0-1.084 1.083ZM22.75 9.685a1.422 1.422 0 0 0-.065-.292v-.098a1.16 1.16 0 0 0-.206-.303l-6.5-6.5a1.16 1.16 0 0 0-.303-.206.347.347 0 0 0-.098 0 .953.953 0 0 0-.357-.12h-4.388a3.25 3.25 0 0 0-3.25 3.25V16.25a3.25 3.25 0 0 0 3.25 3.25H19.5a3.25 3.25 0 0 0 3.25-3.25V9.685Zm-6.5-3.824 2.806 2.806h-1.723a1.083 1.083 0 0 1-1.083-1.084V5.861Zm4.333 10.39a1.083 1.083 0 0 1-1.083 1.082h-8.667A1.083 1.083 0 0 1 9.75 16.25V5.417a1.083 1.083 0 0 1 1.083-1.084h3.25v3.25c.003.37.07.736.195 1.084h-2.361a1.083 1.083 0 1 0 0 2.166h8.666v5.417Z" />
+                    </svg>
+                    <span class="font-medium text-gray-300">Master</span>
+                </a>
+                <a href="#" class="flex flex-row rounded-lg px-4 py-3 space-x-7 ">
+                    <div class="w-6 h-6">
+                    </div>
+                    <span class="text-gray-300">Kategori Produk</span>
+                </a>
+                <a href="#" class="flex flex-row rounded-lg px-4 py-3 space-x-7 ">
+                    <div class="w-6 h-6">
+                    </div>
+                    <span class="text-gray-300">Produk</span>
+                </a>
+                <a href="#" class="flex flex-row rounded-lg px-4 py-3 space-x-7 ">
+                    <div class="w-6 h-6 flex items-stretch">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 6 15" class="w-1.5 h-3.5 self-center mx-auto fill-gray-300">
+                            <path d="M.003 11.27V3.73a.586.586 0 0 1 1-.414l3.77 3.77c.228.229.228.6 0 .828l-3.77 3.77a.586.586 0 0 1-1-.415Z" />
+                        </svg>
+                    </div>
+                    <span class="text-gray-300">Lokasi</span>
+                </a>
+                <a href="#" class="flex flex-row rounded-lg px-4 py-3 space-x-7 ">
+                    <div class="w-6 h-6">
+                    </div>
+                    <span class="text-gray-300">Kategori Produk</span>
+                </a>
+                <a href="#" class="flex flex-row rounded-lg px-4 py-3 space-x-7 ">
+                    <div class="w-6 h-6">
+                    </div>
+                    <span class="text-gray-300">Kategori Produk</span>
+                </a>
+                <a href="#" class="flex flex-row rounded-lg px-4 py-3 space-x-7 ">
+                    <div class="w-6 h-6">
+                    </div>
+                    <span class="text-gray-300">Kategori Produk</span>
+                </a>
+                <a href="#" class="flex flex-row rounded-lg px-4 py-3 space-x-7 ">
+                    <div class="w-6 h-6">
+                    </div>
+                    <span class="text-gray-300">Kategori Produk</span>
+                </a>
+            </div>
+        </div>
+    </aside>
