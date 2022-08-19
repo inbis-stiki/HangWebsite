9652aa6dade9c37dd9711f97e6937bb1366b408b
ImandaVerliefd <verliefd87@gmail.com>
2022-08-19 10:59:30 +0700

    faktur web

--- resources/views/master/faktur/detail_faktur.blade.php
+++ resources/views/master/faktur/detail_faktur.blade.php
@@ -23,10 +23,34 @@
                     <div class="card-body">
                         <div class="row">
                             <div class="col-xl-6">
-                                <div class="grid-col mb-4">.Col-lg-06</div>
+                                <div class="grid mb-4">
+                                    <span class="fs-20 text-black d-block mb-3">Nama User</span>
+                                    <p class="fs-18 ml-4"><?= $faktur->NAME_USER ?></p>
+                                    <span class="fs-20 text-black d-block mb-3">Aktifitas</span>
+                                    <p class="fs-18 ml-4"><?= $faktur->NAME_TYPE ?></p>
+                                    <span class="fs-20 text-black d-block mb-3">Location</span>
+                                    <p class="fs-18 ml-4"><?= $faktur->LOCATION_TD ?></p>
+                                    <span class="fs-20 text-black d-block mb-3">Area</span>
+                                    <p class="fs-18 ml-4"><?= $faktur->AREA_TD ?></p>
+                                    <span class="fs-20 text-black d-block mb-3">List Barang</span>
+                                        
+                                        <p class="fs-18 ml-4">Pax</p>
+                                        
+                                    <span class="fs-20 text-black d-block mb-3">Total Harga</span>
+                                    <p class="fs-18 ml-4">IDR <?= $faktur->TOTAL_TD ?></p>
+                                    <span class="fs-20 text-black d-block mb-3">Regional</span>
+                                    <p class="fs-18 ml-4"><?= $faktur->REGIONAL_TD ?></p>
+                                    <span class="fs-20 text-black d-block mb-3">Tanggal Faktur</span>
+                                    <p class="fs-18 ml-4"><?= $faktur->DATEFACTUR_TD ? date_format(date_create($faktur->DATEFACTUR_TD), 'j F Y H:i') : "" ?></p>
+                                </div>
                             </div>
                             <div class="col-xl-6">
-                                <div class="grid-col mb-4">.Col-lg-06</div>
+                                <div class="grid mb-4">
+                                    @php $img = explode(';', $faktur->FACTUR_TD); @endphp
+                                    <span class="fs-20 text-black d-block mb-3">Foto Faktur</span>
+                                    <img src="{{$img[0]}}" style="max-width: 300px; margin-bottom: 10px" alt="">
+                                    <img src="{{$img[1]}}" style="max-width: 300px; margin-bottom: 10px" alt="">
+                                </div>
                             </div>
                         </div>
                     </div>
