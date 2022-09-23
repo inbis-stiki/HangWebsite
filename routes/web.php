2a03415fc35cbbf2219cdf88b2494993280972e3
devisap <ilham.sagitaputra@gmail.com>
2022-09-23 14:02:53 +0700

    fixing api cronjob ranking user activity n achievement

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::post('auth', 'AuthController@auth');
 Route::get('logout', 'AuthController@logout');
 Route::get('cronjob/update-shop-recommendation', 'CronjobController@updateRecommendShop');
 Route::get('cronjob/rangking', 'CronjobController@cronjob_template_rangking');
-Route::get('cronjob/update-daily-rangking', 'CronjobController@updateDailyRanking');
+Route::get('cronjob/update-daily-rangking-achievement', 'CronjobController@updateDailyRankingAchievement');
 Route::get('cronjob/update-daily-rangking-activity', 'CronjobController@updateDailyRankingActivity');
 Route::get('cronjob/aktivitasrpodapul', 'CronjobController@AktivitasRPODapul');
 Route::get('cronjob/aktivitasrpolapul', 'CronjobController@AktivitasRPOLapul');
