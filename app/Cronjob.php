ea6ac153380e405c1ec5432d8c6741333f0fee7f
devisap <ilham.sagitaputra@gmail.com>
2022-12-27 10:05:26 +0700

    generate rank rpo

--- app/Cronjob.php
+++ app/Cronjob.php
@@ -119,126 +119,192 @@ class Cronjob extends Model
             GROUP BY t.AREA_TRANS 
         ");
     }
-    public static function queryGetSmyRegional($year, $month){
-        $reportProds = DB::select("
+    public static function queryGetSmy($year, $month, $area, $isInside){
+        // SET WEIGHT
+        $wUST       = CategoryProduct::where("ID_PC", "12")->first()->PERCENTAGE_PC;
+        $wNONUST    = CategoryProduct::where("ID_PC", "2")->first()->PERCENTAGE_PC;
+        $wSELERAKU  = CategoryProduct::where("ID_PC", "3")->first()->PERCENTAGE_PC;
+        
+        $wUB        = ActivityCategory::where("ID_AC", "1")->first()->PERCENTAGE_AC;
+        $wPS        = ActivityCategory::where("ID_AC", "2")->first()->PERCENTAGE_AC;
+        $wRETAIL    = ActivityCategory::where("ID_AC", "3")->first()->PERCENTAGE_AC;
+        // SET AREA
+        if($area == "Regional"){
+            $groupBy = "GROUP BY stl.REGIONAL_STL";
+            $nameUser = "
+                SELECT DISTINCT(u.NAME_USER)
+                FROM md_regional mr 
+                JOIN `user` u  
+                    ON 
+                        mr.NAME_REGIONAL = smy.REGIONAL_STL COLLATE utf8mb4_unicode_ci
+                        AND u.ID_REGIONAL = mr.ID_REGIONAL 
+                        AND u.ID_ROLE = 4
+            ";
+            // SET TARGET
+            $tgtUB      = (12 * 14); // (tgt * jmlarea)
+            $tgtPS      = (14 * 3) * 25; // (tgt * jmluser) * totharikerja
+            $tgtRetail  = (14 * 3) * 25; // (tgt * jmluser) * totharikerja
+
+            $tgtUST         = (80 * 3) * 25;
+            $tgtNONUST      = (1000 * 3) * 25;
+            $tgtSeleraku    = (180 * 3) * 25;
+        }else if($area == "Location"){
+            $groupBy = "GROUP BY stl.LOCATION_STL";
+            $nameUser = "
+                SELECT DISTINCT(u.NAME_USER)
+                FROM md_regional mr 
+                JOIN `user` u  
+                    ON 
+                        ml.NAME_LOCATION = smy.LOCATION_STL COLLATE utf8mb4_unicode_ci
+                        AND u.ID_LOCATION = ml.ID_LOCATION 
+                        AND u.ID_ROLE = 3
+            ";
+
+            // SET TARGET
+            $tgtUB      = (12 * 14) * 2; // (tgt * jmlarea) * totasmen
+            $tgtPS      = ((14 * 3) * 25) * 2; // ((tgt * jmluser) * totharikerja) * totasmen
+            $tgtRetail  = (14 * 3) * 25; // ((tgt * jmluser) * totharikerja) * totasmen
+
+            $tgtUST         = ((80 * 3) * 25) * 2;
+            $tgtNONUST      = ((1000 * 3) * 25) * 2;
+            $tgtSeleraku    = ((180 * 3) * 25) * 2;
+        }
+
+        // SET DAPUL OR LAPUL
+        if($isInside == "1"){
+            $isInsideQry = "AND ml.ISINSIDE_LOCATION = 1";
+        }else{
+            $isInsideQry = "AND ml.ISINSIDE_LOCATION = 0";
+        }
+
+        $resProds = DB::select("
             SELECT 
-                smy.*,
-                ((smy.VSUST * smy.WUST) / 100) + ((smy.VSNONUST * smy.WNONUST) / 100) + ((smy.VSSELERAKU * smy.WSELERAKU) / 100) as AVG_VS
+                ( ".$nameUser." ) as NAME_USER,
+                smy.LOCATION_STL,
+                smy.REGIONAL_STL,
+                smy.REALUST_STL ,
+                (".$tgtUST.") as TGTUST ,
+                smy.VSUST ,
+                smy.REALNONUST_STL ,
+                (".$tgtNONUST.") as TGTNONUST ,
+                smy.VSNONUST ,
+                smy.REALSELERAKU_STL ,
+                (".$tgtSeleraku.") as TGTSELERAKU ,
+                smy.VSSELERAKU ,
+                ((smy.VSUST * ".$wUST.") / 100) + ((smy.VSNONUST * ".$wNONUST.") / 100) + ((smy.VSSELERAKU * ".$wSELERAKU.") / 100) as AVG_VS
             FROM (
                 SELECT
                     stl.*,
                     (
-                        SELECT mpc.PERCENTAGE_PC
-                        FROM md_product_category mpc 
-                        WHERE mpc.ID_PC = 12
-                    ) as WUST,
-                    (
-                        SELECT mpc.PERCENTAGE_PC
-                        FROM md_product_category mpc 
-                        WHERE mpc.ID_PC = 2
-                    ) as WNONUST,
-                    (
-                        SELECT mpc.PERCENTAGE_PC
-                        FROM md_product_category mpc 
-                        WHERE mpc.ID_PC = 3
-                    ) as WSELERAKU,
-                    (
-                        SELECT mpc.TGTREGIONAL_PC
-                        FROM md_product_category mpc 
-                        WHERE mpc.ID_PC = 12
-                    ) as TGTUST,
-                    (
-                        SELECT mpc.TGTREGIONAL_PC
-                        FROM md_product_category mpc 
-                        WHERE mpc.ID_PC = 2
-                    ) as TGTNONUST,
-                    (
-                        SELECT mpc.TGTREGIONAL_PC
-                        FROM md_product_category mpc 
-                        WHERE mpc.ID_PC = 3
-                    ) as TGTSELERAKU,
-                    (
-                        SELECT (stl.REALUST_STL / mpc.TGTREGIONAL_PC) * 100
-                        FROM md_product_category mpc 
-                        WHERE mpc.ID_PC = 12
+                        (stl.REALUST_STL / (".$tgtUST.")) * 100
                     ) as VSUST,
                     (
-                        SELECT (stl.REALNONUST_STL / mpc.TGTREGIONAL_PC) * 100
-                        FROM md_product_category mpc 
-                        WHERE mpc.ID_PC = 2
+                        (stl.REALNONUST_STL / (".$tgtNONUST.")) * 100
                     ) as VSNONUST,
                     (
-                        SELECT (stl.REALSELERAKU_STL / mpc.TGTREGIONAL_PC) * 100
-                        FROM md_product_category mpc 
-                        WHERE mpc.ID_PC = 3
+                        (stl.REALSELERAKU_STL / (".$tgtSeleraku.")) * 100
                     ) as VSSELERAKU
                 FROM summary_trans_location stl
-                WHERE
-                    stl.YEAR_STL = ".$year."
-                    AND stl.MONTH_STL = ".$month."
+                INNER JOIN md_location ml
+                    ON 
+                        stl.YEAR_STL = ".$year."
+                        AND stl.MONTH_STL = ".$month."
+                        AND ml.NAME_LOCATION = stl.LOCATION_STL COLLATE utf8mb4_unicode_ci
+                        ".$isInsideQry."
+                    ".$groupBy."
             ) as smy
             ORDER BY AVG_VS DESC
         ");
 
-        $reportActs = DB::select("
+        $resActs = DB::select("
             SELECT 
-                smy.*,
-                ((smy.VSUB * smy.WUB) / 100) + ((smy.VSPS * smy.WPS) / 100) + ((smy.VSRETAIL * smy.WRETAIL) / 100) as AVG_VS
+                ( ".$nameUser." ) as NAME_USER,
+                smy.LOCATION_STL,
+                smy.REGIONAL_STL,
+                smy.REALACTUB_STL ,
+                (".$tgtUB.") as TGTUB ,
+                smy.VSUB ,
+                smy.REALACTPS_STL ,
+                (".$tgtPS.") as TGTPS ,
+                smy.VSPS ,
+                smy.REALACTRETAIL_STL ,
+                (".$tgtRetail.") as TGTRETAIL ,
+                smy.VSRETAIL ,
+                ((smy.VSUB * ".$wUB.") / 100) + ((smy.VSPS * ".$wPS.") / 100) + ((smy.VSRETAIL * ".$wRETAIL.") / 100) as AVG_VS
             FROM (
                 SELECT
                     stl.*,
                     (
-                        SELECT mac.PERCENTAGE_AC 
-                        FROM md_activity_category mac  
-                        WHERE mac.ID_AC = 1
-                    ) as WUB,
-                    (
-                        SELECT mac.PERCENTAGE_AC 
-                        FROM md_activity_category mac  
-                        WHERE mac.ID_AC = 2
-                    ) as WPS,
-                    (
-                        SELECT mac.PERCENTAGE_AC 
-                        FROM md_activity_category mac  
-                        WHERE mac.ID_AC = 3
-                    ) as WRETAIL,
-                    (
-                        SELECT mac.TGTREGIONAL_AC 
-                        FROM md_activity_category mac  
-                        WHERE mac.ID_AC = 1
-                    ) as TGTUB,
-                    (
-                        SELECT mac.TGTREGIONAL_AC 
-                        FROM md_activity_category mac  
-                        WHERE mac.ID_AC = 2
-                    ) as TGTPS,
-                    (
-                        SELECT mac.TGTREGIONAL_AC 
-                        FROM md_activity_category mac  
-                        WHERE mac.ID_AC = 3
-                    ) as TGTRETAIL,
-                    (
-                        SELECT (stl.REALACTUB_STL / mac.TGTREGIONAL_AC) * 100
-                        FROM md_activity_category mac
-                        WHERE mac.ID_AC = 1
+                        (stl.REALACTUB_STL / (".$tgtUB.")) * 100
                     ) as VSUB,
                     (
-                        SELECT (stl.REALACTPS_STL / mac.TGTREGIONAL_AC) * 100
-                        FROM md_activity_category mac
-                        WHERE mac.ID_AC = 2
+                        (stl.REALACTUB_STL / (".$tgtPS.")) * 100
                     ) as VSPS,
                     (
-                        SELECT (stl.REALACTRETAIL_STL / mac.TGTREGIONAL_AC) * 100
-                        FROM md_activity_category mac
-                        WHERE mac.ID_AC = 3
+                        (stl.REALACTUB_STL / (".$tgtRetail.")) * 100
                     ) as VSRETAIL
                 FROM summary_trans_location stl
-                WHERE
-                    stl.YEAR_STL = ".$year."
-                    AND stl.MONTH_STL = ".$month."
+                INNER JOIN md_location ml
+                    ON 
+                        stl.YEAR_STL = ".$year."
+                        AND stl.MONTH_STL = ".$month."
+                        AND ml.NAME_LOCATION = stl.LOCATION_STL COLLATE utf8mb4_unicode_ci
+                        ".$isInsideQry."
+                    ".$groupBy."
             ) as smy
             ORDER BY AVG_VS DESC    
         ");
+
+        $tot1 = 0;
+        $tot2 = 0;
+        $tot3 = 0;
+        foreach ($resProds as $resProd) {
+            $tot1 += $resProd->REALUST_STL;
+            $tot2 += $resProd->REALNONUST_STL;
+            $tot3 += $resProd->REALSELERAKU_STL;
+        }
+        $reportProds['DATAS']                = $resProds;
+        $reportProds['wUST']                 = $wUST;
+        $reportProds['wNONUST']              = $wNONUST;
+        $reportProds['wSELERAKU']            = $wSELERAKU;
+        $reportProds['AVG_REALUST']          = round($tot1 / count($reportProds['DATAS']));
+        $reportProds['AVG_TGTUST']           = $tgtUST;
+        $reportProds['AVG_VSUST']            = $reportProds['AVG_REALUST'] / $reportProds['AVG_TGTUST'];
+        $reportProds['AVG_REALNONUST']       = round($tot2 / count($reportProds['DATAS']));
+        $reportProds['AVG_TGTNONUST']        = $tgtNONUST;
+        $reportProds['AVG_VSNONUST']         = $reportProds['AVG_REALNONUST'] / $reportProds['AVG_TGTNONUST'];
+        $reportProds['AVG_REALSELERAKU']     = round($tot3 / count($reportProds['DATAS']));
+        $reportProds['AVG_TGTSELERAKU']      = $tgtSeleraku;
+        $reportProds['AVG_VSSELERAKU']       = $reportProds['AVG_REALSELERAKU'] / $reportProds['AVG_TGTSELERAKU'];
+        $reportProds['AVG_VS']               = (($reportProds['AVG_VSUST'] * $wUST) / 100);
+        $reportProds['AVG_VS']               += (($reportProds['AVG_VSNONUST'] * $wNONUST) / 100);
+        $reportProds['AVG_VS']               += (($reportProds['AVG_VSSELERAKU'] * $wSELERAKU) / 100);
+        
+        $tot1 = 0;
+        $tot2 = 0;
+        $tot3 = 0;
+        foreach ($resActs as $resAct) {
+            $tot1 += $resAct->REALACTUB_STL;
+            $tot2 += $resAct->REALACTPS_STL;
+            $tot3 += $resAct->REALACTRETAIL_STL;
+        }
+        $reportActs['DATAS']                = $resActs;
+        $reportActs['wUB']                  = $wUB;
+        $reportActs['wPS']                  = $wPS;
+        $reportActs['wRETAIL']              = $wRETAIL;
+        $reportActs['DATAS']                = $resActs;
+        $reportActs['AVG_REALACTUB']        = round($tot1 / count($reportActs['DATAS']));
+        $reportActs['AVG_TGTUB']            = $tgtUB;
+        $reportActs['AVG_VSUB']             = $reportActs['AVG_REALACTUB'] / $reportActs['AVG_TGTUB'];
+        $reportActs['AVG_REALACTPS']        = round($tot2 / count($reportActs['DATAS']));
+        $reportActs['AVG_TGTPS']            = $tgtPS;
+        $reportActs['AVG_VSPS']             = $reportActs['AVG_REALACTPS'] / $reportActs['AVG_TGTPS'];
+        $reportActs['AVG_REALACTRETAIL']    = round($tot3 / count($reportActs['DATAS']));
+        $reportActs['AVG_TGTRETAIL']        = $tgtRetail;
+        $reportActs['AVG_VSRETAIL']         = $reportActs['AVG_REALACTRETAIL'] / $reportActs['AVG_TGTRETAIL'];
+        $reportActs['AVG_VS']               = (($reportActs['AVG_VSUB'] * $wUB) / 100);
+        $reportActs['AVG_VS']               += (($reportActs['AVG_VSPS'] * $wPS) / 100);
+        $reportActs['AVG_VS']               += (($reportActs['AVG_VSRETAIL'] * $wRETAIL) / 100);
 
         return ['reportProds' => $reportProds, 'reportActs' => $reportActs];
     }
