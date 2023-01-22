21bbf28059abcd949bb7b9e617c5a0c9009b7c4f
devisap <ilham.sagitaputra@gmail.com>
2023-01-22 22:29:05 +0700

    fix label monitoring presensi #2

--- resources/views/master/monitoring/monitoring.blade.php
+++ resources/views/master/monitoring/monitoring.blade.php
@@ -41,7 +41,7 @@
                             <div class="row mb-4 mt-2">
                                 <div class="col text-right">
                                     <a href="{{ url('monitoring/download-presence-daily') }}" class="btn btn-primary"><i class="fa fa-download"></i> Laporan {{ date('j/M/Y') }}</a>
-                                    <a href="{{ url('monitoring/download-presence-monthly') }}" class="btn btn-primary"><i class="fa fa-download"></i> Laporan Januari</a>
+                                    <a href="{{ url('monitoring/download-presence-monthly') }}" class="btn btn-primary"><i class="fa fa-download"></i> Laporan {{ dtae('F') }}</a>
                                 </div>
                             </div>
                             <div class="table-responsive">
