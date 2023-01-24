acdfe49c90f655ff2bc52b5e3d3021397ba7ca9e
Zidan8376 <81010981+Zidan8376@users.noreply.github.com>
2023-01-24 21:50:44 +0700

    add pengecekan kecamatan di input toko

--- app/Http/Controllers/api/ShopApi.php
+++ app/Http/Controllers/api/ShopApi.php
@@ -63,6 +63,7 @@ class ShopApi extends Controller
         try {
             $validator = Validator::make($req->all(), [
                 'id_district'           => 'required|numeric|exists:md_district,ID_DISTRICT',
+                'kecamatan'             => 'required|string|exists:md_district,NAME_DISTRICT',
                 'name_shop'             => 'required',
                 'owner_shop'            => 'required',
                 'isinside_market'       => 'required|numeric',
@@ -85,38 +86,51 @@ class ShopApi extends Controller
                     "status_message"    => $validator->errors()->first()
                 ], 400);
             }
-            
+
             $path = $req->file('photo_shop')->store('images', 's3');
 
-            $cek = Shop::where('NAME_SHOP', '=', '' . $req->input('name_shop') . '')
-                ->where('OWNER_SHOP', '=', $req->input('owner_shop'))
-                ->where('TELP_SHOP', '=', $req->input('telp_shop'))
-                ->exists();
+            $district = District::select("ID_DISTRICT")
+                ->where([
+                    ['NAME_DISTRICT', '=', $req->input('kecamatan')],
+                    ['ID_AREA', '=', $req->input('id_area')],
+                    ['ISMARKET_DISTRICT', '=', '0']
+                ])->whereNull('deleted_at')->first();
+            if ($district != null) {
+                $cek = Shop::where('NAME_SHOP', '=', '' . $req->input('name_shop') . '')
+                    ->where('OWNER_SHOP', '=', $req->input('owner_shop'))
+                    ->where('TELP_SHOP', '=', $req->input('telp_shop'))
+                    ->exists();
 
-            if ($cek == true) {
-                return response([
-                    "status_code"       => 400,
-                    "status_message"    => 'Data toko sudah terpakai'
-                ], 200);
+                if ($cek == true) {
+                    return response([
+                        "status_code"       => 400,
+                        "status_message"    => 'Data toko sudah terpakai'
+                    ], 200);
+                } else {
+                    $shop = new Shop();
+                    $shop->ID_DISTRICT          = $req->input('id_district');
+                    $shop->NAME_SHOP            = Str::upper($req->input('name_shop'));
+                    $shop->OWNER_SHOP           = $req->input('owner_shop');
+                    $shop->ISINSIDEMARKET_SHOP  = $req->input('isinside_market');
+                    $shop->TYPE_SHOP            = $req->input('type_shop');
+                    $shop->DETLOC_SHOP          = $req->input('detloc_shop');
+                    $shop->TELP_SHOP            = $req->input('telp_shop');
+                    $shop->LONG_SHOP            = $req->input('long_shop');
+                    $shop->LAT_SHOP             = $req->input('lat_shop');
+                    $shop->ISRECOMMEND_SHOP     = "1";
+                    $shop->PHOTO_SHOP           = Storage::disk('s3')->url($path);
+                    $shop->save();
+
+                    return response([
+                        "status_code"       => 200,
+                        "status_message"    => 'Data berhasil disimpan!',
+                        "data"              => ['ID_SHOP' => $shop->ID_SHOP]
+                    ], 200);
+                }
             } else {
-                $shop = new Shop();
-                $shop->ID_DISTRICT          = $req->input('id_district');
-                $shop->NAME_SHOP            = Str::upper($req->input('name_shop'));
-                $shop->OWNER_SHOP           = $req->input('owner_shop');
-                $shop->ISINSIDEMARKET_SHOP  = $req->input('isinside_market');
-                $shop->TYPE_SHOP            = $req->input('type_shop');
-                $shop->DETLOC_SHOP          = $req->input('detloc_shop');
-                $shop->TELP_SHOP            = $req->input('telp_shop');
-                $shop->LONG_SHOP            = $req->input('long_shop');
-                $shop->LAT_SHOP             = $req->input('lat_shop');
-                $shop->ISRECOMMEND_SHOP     = "1";
-                $shop->PHOTO_SHOP           = Storage::disk('s3')->url($path);
-                $shop->save();
-
                 return response([
-                    "status_code"       => 200,
-                    "status_message"    => 'Data berhasil disimpan!',
-                    "data"              => ['ID_SHOP' => $shop->ID_SHOP]
+                    'status_code'       => 403,
+                    'status_message'    => "Cek lokasi anda!",
                 ], 200);
             }
         } catch (HttpResponseException $exp) {
@@ -130,7 +144,7 @@ class ShopApi extends Controller
     public function list_store(Request $req)
     {
         try {
-            
+
             $validator = Validator::make($req->all(), [
                 'lat_user'           => 'required|numeric',
                 'lng_user'             => 'required|numeric'
@@ -164,7 +178,6 @@ class ShopApi extends Controller
             }
 
             $id_district = $CheckPresence->ID_DISTRICT;
-
             $shop = DB::table("md_shop")
                 ->select("md_shop.*")
                 ->selectRaw("ST_DISTANCE_SPHERE(
@@ -222,7 +235,7 @@ class ShopApi extends Controller
     public function list_store_rekomendasi(Request $req)
     {
         try {
-            
+
             $validator = Validator::make($req->all(), [
                 'lat_user'           => 'required|numeric',
                 'lng_user'             => 'required|numeric'
@@ -263,14 +276,15 @@ class ShopApi extends Controller
                     point('$longitude', '$latitude'),
                     point(md_shop.LONG_SHOP, md_shop.LAT_SHOP)
                 )  AS DISTANCE_SHOP")
-                ->where([
+                ->where(
+                    [
                         ['md_shop.ID_DISTRICT', '=', $id_district],
                         ['md_shop.ISRECOMMEND_SHOP', '=', 1]
                     ]
                 )
                 ->orderBy('DISTANCE_SHOP', 'asc')
                 ->paginate(10);
-            
+
             $dataPagination = array();
             array_push(
                 $dataPagination,
