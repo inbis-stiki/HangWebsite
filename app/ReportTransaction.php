d7d85a1572f5802223ac0aa5ea878ec9eff45db4
devisap <ilham.sagitaputra@gmail.com>
2023-02-25 05:33:26 +0700

    filename report daily

--- app/ReportTransaction.php
+++ app/ReportTransaction.php
@@ -203,7 +203,7 @@ class ReportTransaction
 
         $spreadsheet->setActiveSheetIndex(0);
 
-        $fileName = 'TRANSAKSI HARIAN - '.$regionalName.' - APO SPG - ' . date_format(date_create(date("Y-m-d")), 'j F Y');
+        $fileName = 'TRANSAKSI HARIAN - '.$regionalName.' - APO SPG - ' . $date;
         $writer = new Xlsx($spreadsheet);
 
         header('Content-Type: application/vnd.ms-excel'); // generate excel file
