bae119240e83a371376b97f8654fac1f50a3e4d0
Panji Iman Baskoro <panjidia995@gmail.com>
2023-04-28 10:15:36 +0700

    Update ReportTransactionController.php

--- app/Http/Controllers/ReportTransactionController.php
+++ app/Http/Controllers/ReportTransactionController.php
@@ -7,12 +7,19 @@ use Illuminate\Http\Request;
 
 class ReportTransactionController extends Controller
 {
-    public function index()
+    public function index(Request $req)
     {
+        $user = $req->session()->get('id_user');
         $data['title']          = "Transaction";
         $data['sidebar']        = "transaction";
         $data['sidebar2']       = "";
-        $data['regionals']      = Regional::where('deleted_at', NULL)->get();
+        $data['regionals']      = DB::table('md_regional')
+                    ->select('md_regional.ID_REGIONAL', 'md_regional.NAME_REGIONAL')
+                    ->join('md_location', 'md_location.ID_LOCATION', '=', 'md_regional.ID_LOCATION')
+                    ->join('user', 'user.ID_LOCATION', '=', 'md_location.ID_LOCATION')
+                    ->where('user.USERNAME_USER', '=', $user)
+                    ->get();
+        
 
         return view('laporan.transaction.transaction', $data);
     }
