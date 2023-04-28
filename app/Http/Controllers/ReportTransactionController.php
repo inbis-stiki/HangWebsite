b677e6565e0d2cc2ac777ac6d50241817a6798f6
Panji Iman Baskoro <panjidia995@gmail.com>
2023-04-28 10:21:27 +0700

    Update ReportTransactionController.php

--- app/Http/Controllers/ReportTransactionController.php
+++ app/Http/Controllers/ReportTransactionController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Regional;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Session;
 
 class ReportTransactionController extends Controller
 {
