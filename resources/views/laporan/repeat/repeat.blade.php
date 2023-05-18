b50f188826e73cb35f958c69102f7436e14ae060
Panji Iman Baskoro <panjidia995@gmail.com>
2023-05-18 21:39:52 +0700

    Update repeat.blade.php

--- resources/views/laporan/repeat/repeat.blade.php
+++ resources/views/laporan/repeat/repeat.blade.php
@@ -179,9 +179,6 @@
                                 <select id="start_year" name="start_year" class="form-control">
                                     <option value="2022">2022</option>
                                     <option value="2023">2023</option>
-                                    <option value="2024">2024</option>
-                                    <option value="2025">2025</option>
-                                    <option value="2026">2026</option>
                                 </select>
                             </div>
                         </div>
@@ -208,9 +205,6 @@
                                 <select id="end_year" name="end_year" class="form-control">
                                     <option value="2022">2022</option>
                                     <option value="2023">2023</option>
-                                    <option value="2024">2024</option>
-                                    <option value="2025">2025</option>
-                                    <option value="2026">2026</option>
                                 </select>
                             </div>
                         </div>
