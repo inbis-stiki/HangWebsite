152527c74b56ac14917a41be74fbd31169e6d7df
budosen <budosen@pop-os.localdomain>
2023-06-02 16:43:31 +0700

    rollback transaction page

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Transaction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -23,67 +22,18 @@ class TransactionController extends Controller
         $tgl_trans  = $req->input('tglSearchtrans');
         $id_role    = $req->session()->get('role');
         $regional   = $req->session()->get('regional');
-        $location   = $req->session()->get('location');
 
-        $RoleJoin = "";
-        $showRole = "";
+        $showRegional = "";
         $showType = "";
-        $tglFilter = "";
-        if ($id_role == 3) {
-            $RoleJoin = "LEFT JOIN md_location ml ON ml.NAME_LOCATION = ml.NAME_LOCATION";
-            $showRole = "AND ml.ID_LOCATION = $location";
-        }
-        if ($id_role == 4 || $id_role == 5 || $id_role == 6) {
-            $RoleJoin = "LEFT JOIN md_regional mr ON mr.NAME_REGIONAL = t.REGIONAL_TRANS";
-            $showRole = "AND mr.ID_REGIONAL = $regional";
+        if($id_role == 3 || $id_role == 4 || $id_role == 5 || $id_role == 6){
+            $showRegional = "AND mr.ID_REGIONAL = $regional";
         }
 
-        if ($id_type != 0) {
+        if($id_type != 0){
             $showType = "AND t.ID_TYPE = $id_type";
         }
 
-        if (!empty($tgl_trans)) {
-            $tglFilter = "AND DATE(t.`DATE_TRANS`) = '" . $tgl_trans . "'";
-        }
-
-        $start = $req->input('start');
-        $perPage = $req->input('length');
-
-        $Tot = DB::select(
-            DB::raw("
-                SELECT 
-                    COUNT(temp.Total) AS DataTrans
-                FROM 
-                    (
-                        SELECT
-                            COUNT(t.ID_TRANS) AS Total,
-                            DATE(t.DATE_TRANS) AS CnvrtDate,
-                            t.AREA_TRANS AS NAME_AREA
-                        FROM
-                            `transaction` t
-                        LEFT JOIN `user` u ON
-                            u.ID_USER = t.ID_USER
-                        LEFT JOIN `md_shop` ms ON
-                            ms.ID_SHOP = t.ID_SHOP
-                        LEFT JOIN `md_type` mt ON
-                            mt.ID_TYPE = t.ID_TYPE
-                        $RoleJoin
-                        WHERE
-                            t.`ISTRANS_TRANS` = 1
-                            $tglFilter
-                            $showRole
-                            $showType
-                        GROUP BY
-                            CnvrtDate,
-                            t.ID_USER,
-                            t.AREA_TRANS
-                        ORDER BY
-                            NAME_AREA ASC
-                    ) temp
-            ")
-        )[0];
-
-        $dataTrans    = DB::select(
+        $dataTrans     = DB::select(
             DB::raw("
                 SELECT
                     t.ID_TRANS ,
@@ -91,44 +41,50 @@ class TransactionController extends Controller
                     t.REGIONAL_TRANS ,
                     t.DATE_TRANS ,
                     t.ID_TYPE ,
-                    t.LOCATION_TRANS AS NAME_LOCATION,
-                    t.REGIONAL_TRANS AS NAME_REGIONAL,
-                    t.AREA_TRANS AS NAME_AREA,
-                    t.KECAMATAN AS NAME_DISTRICT,   
                     u.`ID_USER`,
                     u.`NAME_USER`,
                     u.`ID_ROLE`,
                     mt.`NAME_TYPE`,
                     ms.`LONG_SHOP`,
                     ms.`LAT_SHOP`,
+                    md.`NAME_DISTRICT`,
+                    ma.`NAME_AREA`,
+                    mr.`NAME_REGIONAL`,
+                    ml.`NAME_LOCATION`,
                     DATE(t.DATE_TRANS) AS CnvrtDate,
                     COUNT(t.ID_TRANS) AS TotalTrans
                 FROM
                     `transaction` t
                 LEFT JOIN `user` u ON
-                    u.ID_USER = t.ID_USER
+                    u.`ID_USER` = t.`ID_USER`
                 LEFT JOIN `md_shop` ms ON
-                    ms.ID_SHOP = t.ID_SHOP
+                    ms.`ID_SHOP` = t.`ID_SHOP`
                 LEFT JOIN `md_type` mt ON
-                    mt.ID_TYPE = t.ID_TYPE
-                $RoleJoin
+                    mt.`ID_TYPE` = t.`ID_TYPE`
+                LEFT JOIN `md_district` md ON
+                    md.`ID_DISTRICT` = ms.`ID_DISTRICT`
+                LEFT JOIN `md_area` ma ON
+                    ma.`ID_AREA` = u.`ID_AREA`
+                LEFT JOIN `md_location` ml ON
+                    ml.`ID_LOCATION` = u.`ID_LOCATION`
+                LEFT JOIN `md_regional` mr ON
+                    mr.`ID_REGIONAL` = u.`ID_REGIONAL`
                 WHERE
                     t.`ISTRANS_TRANS` = 1
-                    $tglFilter
-                    $showRole
+                    AND DATE(t.`DATE_TRANS`) = '" . $tgl_trans . "'
+                    $showRegional
                     $showType
                 GROUP BY
                     DATE(t.DATE_TRANS),
                     t.ID_USER,
                     t.ID_TYPE
                 ORDER BY
-                    NAME_AREA ASC
-                LIMIT $start, $perPage
+                    ma.`NAME_AREA` ASC
             ")
         );
 
         $NewData_all = array();
-        $counter_all = $start;
+        $counter_all = 0;
 
         foreach ($dataTrans as $item) {
             $data = array(
@@ -154,14 +110,9 @@ class TransactionController extends Controller
             array_push($NewData_all, $data);
         }
 
-        $draw   = $req->input('draw');
-
         return response([
             'status_code'       => 200,
             'status_message'    => 'Data berhasil diambil!',
-            'draw'              => intval($draw),
-            'recordsFiltered'   => ($counter_all != 0) ? $Tot->DataTrans : 0,
-            'recordsTotal'      => ($counter_all != 0) ? $Tot->DataTrans : 0,
             'data'              => $NewData_all
         ], 200);
     }
