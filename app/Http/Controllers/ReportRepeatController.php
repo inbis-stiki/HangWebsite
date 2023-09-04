ee83cb6f7a2eb06fc168663d5196aaac5e77dae9
budosen <budosen@pop-os.localdomain>
2023-09-04 14:14:08 +0700

    fix lisr refional refer to sessions

--- app/Http/Controllers/ReportRepeatController.php
+++ app/Http/Controllers/ReportRepeatController.php
@@ -4,17 +4,32 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Session;
 
 class ReportRepeatController extends Controller
 {
-    public function index()
+    public function index(Request $req)
     {
+        $user = $req->session()->get('id_user');
         $data['title']          = "";
         $data['sidebar']        = "repeat";
         $data['sidebar2']       = "";
 
-        $data['regional']   = DB::table('md_regional')
-        ->whereNull('deleted_at')->get();
+        if ($user == "e35f88a4" || $user == "8735afgu"){
+            $data['regional']      = DB::table('md_regional')
+            ->select('md_regional.ID_REGIONAL', 'md_regional.NAME_REGIONAL')
+            ->join('md_location', 'md_location.ID_LOCATION', '=', 'md_regional.ID_LOCATION')
+            ->join('user', 'user.ID_LOCATION', '=', 'md_location.ID_LOCATION')
+            ->groupBy('md_regional.NAME_REGIONAL')
+            ->get();
+        }else {
+            $data['regional'] = DB::table('md_regional')
+            ->select('md_regional.ID_REGIONAL', 'md_regional.NAME_REGIONAL')
+            ->join('md_location', 'md_location.ID_LOCATION', '=', 'md_regional.ID_LOCATION')
+            ->join('user', 'user.ID_LOCATION', '=', 'md_location.ID_LOCATION')
+            ->where('user.ID_USER', '=', $user)
+            ->get();
+        }
 
         return view('laporan.repeat.repeat', $data);
     }
--- app/Http/Controllers/ReportTransactionController.php
+++ app/Http/Controllers/ReportTransactionController.php
@@ -16,15 +16,15 @@ class ReportTransactionController extends Controller
         $data['sidebar']        = "transaction";
         $data['sidebar2']       = "";
 
-        if ($user = "e35f88a4" || $user = "8735afgu"){
+        if ($user == "e35f88a4" || $user == "8735afgu"){
             $data['regionals']      = DB::table('md_regional')
             ->select('md_regional.ID_REGIONAL', 'md_regional.NAME_REGIONAL')
             ->join('md_location', 'md_location.ID_LOCATION', '=', 'md_regional.ID_LOCATION')
             ->join('user', 'user.ID_LOCATION', '=', 'md_location.ID_LOCATION')
             ->groupBy('md_regional.NAME_REGIONAL')
             ->get();
-        } else{
-            $data['regionals']      = DB::table('md_regional')
+        }else {
+            $data['regionals'] = DB::table('md_regional')
             ->select('md_regional.ID_REGIONAL', 'md_regional.NAME_REGIONAL')
             ->join('md_location', 'md_location.ID_LOCATION', '=', 'md_regional.ID_LOCATION')
             ->join('user', 'user.ID_LOCATION', '=', 'md_location.ID_LOCATION')
