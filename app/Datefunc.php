dcb6bb468f3c1fdd480153cb27eab6856a6348c7
Panji Iman Baskoro <panjidia995@gmail.com>
2024-06-11 22:45:02 +0700

    Update Datefunc.php

--- app/Datefunc.php
+++ app/Datefunc.php
@@ -14,7 +14,8 @@ class Datefunc
             // $response = Http::get('https://vip.timezonedb.com/v2.1/get-time-zone?key=DHQJPS68JTER&format=json&by=position&lat='.$lat.'&lng='.$long);
             // sleep(3);
 
-            $response = Http::withOptions(['timeout' => 2])->retry(5,5000,null)->get('https://vip.timezonedb.com/v2.1/get-time-zone?key=DHQJPS68JTER&format=json&by=position&lat='.$lat.'&lng='.$long);
+            // $response = Http::withOptions(['timeout' => 2])->retry(5,5000,null)->get('https://vip.timezonedb.com/v2.1/get-time-zone?key=DHQJPS68JTER&format=json&by=position&lat='.$lat.'&lng='.$long);
+            $response = Http::withOptions(['timeout' => 2])->retry(5,5000,null)->get('https://zone.tjireng.my.id/api?lat='.$lat.'&lng='.$long);
 
         } while (!isset($response->json()['zoneName']));
         
