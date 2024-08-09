8f9f0b6704877b79175e76bfec797d827cdeb7de
ImandaVerliefd <verliefd87@gmail.com>
2024-08-09 10:37:43 +0700

    query generate repeat order category

--- app/Http/Controllers/CronjobController.php
+++ app/Http/Controllers/CronjobController.php
@@ -949,6 +949,45 @@ class CronjobController extends Controller
             return 0;
         }
     }
+    public function genROCATShopRange(Request $req)
+    {
+        if (empty($_GET['dateStart']) || empty($_GET['dateEnd'])) {
+            return redirect('laporan/lpr-repeat')->with('err_msg', 'Inputan tanggal awal atau tanggal akhir tidak boleh kosong');
+        } else {
+            $dateStart = explode('-', $_GET['dateStart']);
+            $startY = ltrim($dateStart[0], '0');
+            $startM = ltrim($dateStart[1], '0');
+
+            $dateEnd = explode('-', $_GET['dateEnd']);
+            $endY = ltrim($dateEnd[0], '0');
+            $endM = ltrim($dateEnd[1], '0');
+
+            $idRegional =  $req->input('regional');
+
+            $totalMonth = 0;
+            for ($y = $startY; $y <= $endY; $y++) {
+                for ($m = 1; $m <= 12; $m++) {
+                    if ($y == $startY && $m < $startM) {
+                        continue;
+                    }
+                    if ($y == $endY && $m > $endM) {
+                        continue;
+                    }
+                    $totalMonth++;
+                }
+            }
+
+            if ($idRegional === "null" || empty($idRegional)) {
+                return redirect('laporan/lpr-repeat')->with('err_msg', 'Regional tidak boleh kosong');
+            } elseif ($totalMonth > 12) {
+                return redirect('laporan/lpr-repeat')->with('err_msg', 'Range bulanan tidak boleh lebih dari 12 bulan');
+            } else {
+                $rOs = Cronjob::queryGetShopCatByRange($startM, $startY, $endM, $endY, $idRegional);
+                dd($rOs);
+                app(ReportRepeatOrder::class)->gen_ro_shop_range($rOs, $totalMonth);
+            }
+        }
+    }
     public function genRTPerShop($yearReq)
     {
 
