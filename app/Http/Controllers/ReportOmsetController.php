bd5798b55d8b3ab8309346d6369662326c10ceb2
Zidan8376 <81010981+Zidan8376@users.noreply.github.com>
2024-10-14 14:14:03 +0700

    perbaikan halaman omset

--- app/Http/Controllers/ReportOmsetController.php
+++ app/Http/Controllers/ReportOmsetController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\ReportOmsetHead;
 use App\ReportOmsetDetail;
+use PHPUnit\Framework\Constraint\Count;
 use Session;
 
 class ReportOmsetController extends Controller
@@ -22,21 +23,122 @@ class ReportOmsetController extends Controller
         $id_regional    = $req->session()->get('regional');
 
         if ($id_role == 3) {
-            $data['data_regional']  = DB::table('md_area')
-                ->leftjoin('md_regional', 'md_regional.ID_REGIONAL', '=', 'md_area.ID_REGIONAL')
+            $data['data_regional']  = DB::table('md_regional')
                 ->where('md_regional.ID_LOCATION', '=', $id_location)
                 ->get();
         } else if ($id_role == 4) {
-            $data['data_regional']  = DB::table('md_area')
-                ->leftjoin('md_regional', 'md_regional.ID_REGIONAL', '=', 'md_area.ID_REGIONAL')
+            $data['data_regional']  = DB::table('md_regional')
                 ->where('md_regional.ID_REGIONAL', '=', $id_regional)
                 ->get();
         } else {
-            $data['data_regional']  = DB::table('md_area')->get();
+            $data['data_regional']  = DB::table('md_regional')->get();
         }
 
+        $data['shop_prod'] = $this->shop_prod_kat();
+
+        // $year = 2024;
+        // $data['result'] = DB::table('user as u')
+        //     ->select(
+        //         'u.NAME_USER',
+        //         DB::raw('SUM(CASE WHEN h.BULAN = 1 THEN d.TOTAL_OMSET ELSE 0 END) AS MONTH1_TOTAL_OMSET'),
+        //         DB::raw('SUM(CASE WHEN h.BULAN = 1 THEN d.TOTAL_OUTLET ELSE 0 END) AS MONTH1_TOTAL_OUTLET'),
+        //         DB::raw('SUM(CASE WHEN h.BULAN = 2 THEN d.TOTAL_OMSET ELSE 0 END) AS MONTH2_TOTAL_OMSET'),
+        //         DB::raw('SUM(CASE WHEN h.BULAN = 2 THEN d.TOTAL_OUTLET ELSE 0 END) AS MONTH2_TOTAL_OUTLET'),
+        //         DB::raw('SUM(CASE WHEN h.BULAN = 3 THEN d.TOTAL_OMSET ELSE 0 END) AS MONTH3_TOTAL_OMSET'),
+        //         DB::raw('SUM(CASE WHEN h.BULAN = 3 THEN d.TOTAL_OUTLET ELSE 0 END) AS MONTH3_TOTAL_OUTLET'),
+        //         DB::raw('SUM(CASE WHEN h.BULAN = 4 THEN d.TOTAL_OMSET ELSE 0 END) AS MONTH4_TOTAL_OMSET'),
+        //         DB::raw('SUM(CASE WHEN h.BULAN = 4 THEN d.TOTAL_OUTLET ELSE 0 END) AS MONTH4_TOTAL_OUTLET'),
+        //         DB::raw('SUM(CASE WHEN h.BULAN = 5 THEN d.TOTAL_OMSET ELSE 0 END) AS MONTH5_TOTAL_OMSET'),
+        //         DB::raw('SUM(CASE WHEN h.BULAN = 5 THEN d.TOTAL_OUTLET ELSE 0 END) AS MONTH5_TOTAL_OUTLET'),
+        //         DB::raw('SUM(CASE WHEN h.BULAN = 6 THEN d.TOTAL_OMSET ELSE 0 END) AS MONTH6_TOTAL_OMSET'),
+        //         DB::raw('SUM(CASE WHEN h.BULAN = 6 THEN d.TOTAL_OUTLET ELSE 0 END) AS MONTH6_TOTAL_OUTLET'),
+        //         DB::raw('SUM(CASE WHEN h.BULAN = 7 THEN d.TOTAL_OMSET ELSE 0 END) AS MONTH7_TOTAL_OMSET'),
+        //         DB::raw('SUM(CASE WHEN h.BULAN = 7 THEN d.TOTAL_OUTLET ELSE 0 END) AS MONTH7_TOTAL_OUTLET'),
+        //         DB::raw('SUM(CASE WHEN h.BULAN = 8 THEN d.TOTAL_OMSET ELSE 0 END) AS MONTH8_TOTAL_OMSET'),
+        //         DB::raw('SUM(CASE WHEN h.BULAN = 8 THEN d.TOTAL_OUTLET ELSE 0 END) AS MONTH8_TOTAL_OUTLET'),
+        //         DB::raw('SUM(CASE WHEN h.BULAN = 9 THEN d.TOTAL_OMSET ELSE 0 END) AS MONTH9_TOTAL_OMSET'),
+        //         DB::raw('SUM(CASE WHEN h.BULAN = 9 THEN d.TOTAL_OUTLET ELSE 0 END) AS MONTH9_TOTAL_OUTLET'),
+        //         DB::raw('SUM(CASE WHEN h.BULAN = 10 THEN d.TOTAL_OMSET ELSE 0 END) AS MONTH10_TOTAL_OMSET'),
+        //         DB::raw('SUM(CASE WHEN h.BULAN = 10 THEN d.TOTAL_OUTLET ELSE 0 END) AS MONTH10_TOTAL_OUTLET'),
+        //         DB::raw('SUM(CASE WHEN h.BULAN = 11 THEN d.TOTAL_OMSET ELSE 0 END) AS MONTH11_TOTAL_OMSET'),
+        //         DB::raw('SUM(CASE WHEN h.BULAN = 11 THEN d.TOTAL_OUTLET ELSE 0 END) AS MONTH11_TOTAL_OUTLET'),
+        //         DB::raw('SUM(CASE WHEN h.BULAN = 12 THEN d.TOTAL_OMSET ELSE 0 END) AS MONTH12_TOTAL_OMSET'),
+        //         DB::raw('SUM(CASE WHEN h.BULAN = 12 THEN d.TOTAL_OUTLET ELSE 0 END) AS MONTH12_TOTAL_OUTLET')
+        //     )
+        //     ->leftJoin('report_omset_detail as d', DB::raw('u.ID_USER COLLATE utf8mb4_general_ci'), '=', 'd.ID_USER')
+        //     ->leftJoin('report_omset_head as h', 'd.ID_HEAD', '=', 'h.ID_HEAD')
+        //     ->whereNull('u.deleted_at')
+        //     ->where(function ($query) use ($year) {
+        //         $query->where('h.TAHUN', $year)
+        //             ->orWhereNull('h.TAHUN');
+        //     })
+        //     ->groupBy('u.ID_USER', 'u.NAME_USER')
+        //     ->orderBy('u.ID_USER')
+        //     ->get();
+        // dd($data['result']);
+
+        return view('laporan.omset.omset', $data);
+    }
+
+    public function shop_prod_kat()
+    {
+        $data = DB::select("
+            SELECT
+                mts.ID_TYPE AS ID_CAT,
+                mts.NAME_TYPE COLLATE utf8mb4_general_ci AS NAMA ,
+                'SHOP_CATEGORY' AS `TYPE`
+            FROM 
+                md_type_shop mts 
+            UNION ALL
+            SELECT 
+                mpc.ID_PC AS ID_CAT,
+                mpc.NAME_PC COLLATE utf8mb4_general_ci AS NAMA ,
+                'PRODUCT_CATEGORY' AS `TYPE`
+            FROM 
+                md_product_category mpc
+        ");
+
+        $groupedData = [];
+        foreach ($data as $item) {
+            $groupedData[$item->TYPE][] = $item;
+        }
+
+        return $groupedData;
+    }
+
+    public function pipeline_datatable(Request $req)
+    {
+        $regional   = $req->input('regional');
+        $shopProduct  = $req->input('shopProduct');
+        $typeshopProduct  = $req->input('typeShopProduct');
+        $search     = $req->input('search')['value'];
+
+        $condition = [];
+        if (!empty($regional)) {
+            $condition[] = "h.ID_REGIONAL = '$regional'";
+        }
+        if (!empty($typeshopProduct) && !empty($shopProduct)) {
+            if ($typeshopProduct == 'SHOP_CATEGORY') {
+                $condition[] = "d.TYPE_SHOP = '$shopProduct'";
+            }
+            if ($typeshopProduct == 'PRODUCT_CATEGORY') {
+                $condition[] = "d.ID_PC = '$shopProduct'";
+            }
+        }
+        if (!empty($search)) {
+            $condition[] = "u.NAME_USER LIKE '%$search%'";
+        }
+
+        if (!empty($condition)) {
+            $conditionWhere = implode(' AND ', $condition);
+        }
+
+        $start = $req->input('start');
+        $perPage = $req->input('length');
+        $draw = $req->input('draw');
+
         $year = 2024;
-        $data['result'] = DB::table('user as u')
+        $result = DB::table('user as u')
             ->select(
                 'u.NAME_USER',
                 DB::raw('SUM(CASE WHEN h.BULAN = 1 THEN d.TOTAL_OMSET ELSE 0 END) AS MONTH1_TOTAL_OMSET'),
@@ -69,13 +171,44 @@ class ReportOmsetController extends Controller
             ->whereNull('u.deleted_at')
             ->where(function ($query) use ($year) {
                 $query->where('h.TAHUN', $year)
-                      ->orWhereNull('h.TAHUN');
+                    ->orWhereNull('h.TAHUN');
             })
             ->groupBy('u.ID_USER', 'u.NAME_USER')
-            ->orderBy('u.ID_USER')
-            ->get();
-        // dd($data['result']);
+            ->orderBy('u.ID_USER');
 
-        return view('laporan.omset.omset', $data);
+        if (!empty($condition)) {
+            $result->whereRaw($conditionWhere);
+        }
+
+        $resultAll = count($result->get());
+        $resultLimit = $result
+            ->offset($start)
+            ->limit($perPage)
+            ->get();
+
+        $NewData_all = array();
+        $counter_all = $start;
+
+        foreach ($resultLimit as $item) {
+            ++$counter_all;
+            $data = array(
+                "NAME_USER" => $item->NAME_USER,
+            );
+
+            for ($index = 1; $index <= 12; $index++) {
+                $data['TOTAL_OMSET_' . $index] = $item->{'MONTH' . $index . '_TOTAL_OMSET'};
+                $data['JML_OUTLET_' . $index] = $item->{'MONTH' . $index . '_TOTAL_OUTLET'};
+            }
+            array_push($NewData_all, $data);
+        }
+
+        return response([
+            'status_code'       => 200,
+            'status_message'    => 'Data berhasil diambil!',
+            'draw'              => intval($draw),
+            'recordsFiltered'   => ($counter_all != 0) ? $resultAll : 0,
+            'recordsTotal'      => ($counter_all != 0) ? $resultAll : 0,
+            'data'              => $NewData_all
+        ], 200);
     }
 }
--- resources/views/laporan/omset/omset.blade.php
+++ resources/views/laporan/omset/omset.blade.php
@@ -21,14 +21,42 @@
                 <div class="card">
                     <div class="card-body">
                         <div class="row">
-                            <div class="col-6">
-                                <h4 class="card-title">Regional Presensi</h4>
+                            <div class="col-4">
+                                <h4 class="card-title">Regional</h4>
                                 <select name="transaksi" id="SelectRegional" class="select2">
-                                    <option selected value="0">All Regional</option>
+                                    <option selected value="">All Regional</option>
                                     @foreach($data_regional as $item)
-                                    <option value="{{$item->ID_AREA}}">{{$item->NAME_AREA}}</option>
+                                    <option value="{{$item->NAME_REGIONAL}}">{{$item->NAME_REGIONAL}}</option>
                                     @endforeach
                                 </select>
+                            </div>
+                            <div class="col-6">
+                                <h4 class="card-title">Tipe Toko / Kategori Produk</h4>
+                                <select name="shopProd" id="SelectShopKat" class="select2">
+                                    <option selected value="">Show All</option>
+                                    @foreach($shop_prod as $key => $item)
+                                    @if($key == 'SHOP_CATEGORY')
+                                    <optgroup label="Tipe Toko">
+                                        @foreach($item as $itemChild)
+                                        <option value="{{$itemChild->NAMA}}" data-tipeFilter='{{$itemChild->TYPE}}'>{{$itemChild->NAMA}}</option>
+                                        @endforeach
+                                    </optgroup>
+                                    @endif
+                                    @if($key == 'PRODUCT_CATEGORY')
+                                    <optgroup label="Kategori Produk">
+                                        @foreach($item as $itemChild)
+                                        <option value="{{$itemChild->ID_CAT}}" data-tipeFilter='{{$itemChild->TYPE}}'>{{$itemChild->NAMA}}</option>
+                                        @endforeach
+                                    </optgroup>
+                                    @endif
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="col-2">
+                                <div class="d-flex align-items-end justify-content-end h-100">
+                                    <button type="button" id="btn-filter-omset" class="btn btn-primary mr-3"><i class="fa-solid fa-filter"></i></button>
+                                    <button type="button" id="btn-download-omset" class="btn btn-primary"><i class="fa-solid fa-file-arrow-down"></i></button>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -36,43 +64,31 @@
             </div>
         </div>
 
+        <?php $indMonth = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember']; ?>
         <!-- Add Order -->
         <div class="row">
             <div class="col-12">
                 <div class="card">
-                    {{-- <div class="card-header">
-                        <h4 class="card-title">Daftar Omset</h4>
-                    </div> --}}
                     <div class="card-body">
                         <div class="table-responsive">
-                        <table id="datatable" class="display min-w850">
-                            <thead>
-                                <tr>
-                                    <th>APO/SPG</th>
-                                    @for ($month = 1; $month <= 12; $month++)
-                                        <th colspan="2">{{ $month }}</th>
-                                    @endfor
-                                </tr>
-                                <tr>
-                                    <th></th>
-                                    @for ($month = 1; $month <= 12; $month++)
-                                        <th>Total Omset</th>
-                                        <th>Jumlah Outlet</th>
-                                    @endfor
-                                </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($result as $user)
-                                <tr>
-                                    <td>{{ $user->NAME_USER }}</td>
-                                    @for ($month = 1; $month <= 12; $month++)
-                                        <td>{{ $user->{'MONTH' . $month . '_TOTAL_OMSET'} ?? 0 }}</td>
-                                        <td>{{ $user->{'MONTH' . $month . '_TOTAL_OUTLET'} ?? 0 }}</td>
-                                    @endfor
-                                </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
+                            <table id="datatable" class="table-bordered display min-w850">
+                                <thead>
+                                    <tr>
+                                        <th rowspan="2" style="vertical-align: middle !important; width: 99px !important; background-color: white !important;">APO/SPG</th>
+                                        @for ($month = 1; $month <= 12; $month++)
+                                            <th colspan="2" class="text-center">{{ $indMonth[($month - 1)] }}</th>
+                                            @endfor
+                                    </tr>
+                                    <tr>
+                                        @for ($month = 1; $month <= 12; $month++)
+                                            <th>Total Omset</th>
+                                            <th>Jumlah Outlet</th>
+                                            @endfor
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
@@ -80,9 +96,207 @@
         </div>
     </div>
 </div>
+<style>
+    .dataTables_processing {
+        top: 0 !important;
+        left: 0 !important;
+        width: 100% !important;
+        height: 100% !important;
+        background: rgba(0, 0, 0, 0.5) !important;
+        z-index: 10000 !important;
+        justify-content: center !important;
+        align-items: center !important;
+        margin-left: 0px !important;
+        margin-top: 0px !important;
+        text-align: center !important;
+        padding: 0px !important;
+    }
+
+    .prevent-horizontal-scroll {
+        overflow-x: hidden !important;
+    }
+
+    table.dataTable.display tbody tr:hover td {
+        background-color: white !important;
+    }
+</style>
 @include('template/footer')
 <script>
-    $(document).ready(function() {
-        $('#datatable').DataTable();
+    var regional = $('#SelectRegional').val()
+    var shopProduct = $('#SelectShopKat').val()
+    var typeShopProduct = $('#SelectShopKat').find(':selected').attr('data-tipeFilter')
+    const tableElem = $('#datatable')
+
+    var customColumn = [{
+        data: 'NAME_USER'
+    }]
+
+    for (let index = 1; index <= 12; index++) {
+        customColumn.push({
+            data: 'TOTAL_OMSET_' + index,
+            mData: 'TOTAL_OMSET_' + index
+        }, {
+            data: 'JML_OUTLET_' + index,
+            mData: 'JML_OUTLET_' + index
+        })
+    }
+
+    $.fn.dataTable.pipeline = function(opts) {
+        var conf = $.extend(opts);
+
+        var cacheLower = -1;
+        var cacheUpper = null;
+        var cacheLastRequest = null;
+        var cacheLastJson = null;
+
+        return function(request, drawCallback, settings) {
+            var ajax = false;
+            var requestStart = request.start;
+            var drawStart = request.start;
+            var requestLength = request.length;
+            var requestEnd = requestStart + requestLength;
+
+            if (settings.clearCache) {
+                ajax = true;
+                settings.clearCache = false;
+            } else if (cacheLower < 0 || requestStart < cacheLower || requestEnd > cacheUpper) {
+                ajax = true;
+            } else if (
+                JSON.stringify(request.order) !== JSON.stringify(cacheLastRequest.order) ||
+                JSON.stringify(request.columns) !== JSON.stringify(cacheLastRequest.columns) ||
+                JSON.stringify(request.search) !== JSON.stringify(cacheLastRequest.search)
+            ) {
+                ajax = true;
+            }
+
+            cacheLastRequest = $.extend(true, {}, request);
+
+            if (ajax) {
+                if (requestStart < cacheLower) {
+                    requestStart = requestStart - requestLength * (conf.pages - 1);
+                    if (requestStart < 0) {
+                        requestStart = 0;
+                    }
+                }
+
+                cacheLower = requestStart;
+                cacheUpper = requestStart + requestLength * conf.pages;
+
+                request.start = requestStart;
+                request.length = requestLength * conf.pages;
+
+
+                if (typeof conf.data === 'function') {
+                    var d = conf.data(request);
+                    if (d) {
+                        $.extend(request, d);
+                    }
+                } else if ($.isPlainObject(conf.data)) {
+
+                    $.extend(request, conf.data);
+                }
+
+                return $.ajax({
+                    type: conf.method,
+                    url: conf.url,
+                    data: request,
+                    dataType: 'json',
+                    cache: false,
+                    success: function(json) {
+                        cacheLastJson = $.extend(true, {}, json);
+
+                        if (cacheLower != drawStart) {
+                            json.data.splice(0, drawStart - cacheLower);
+                        }
+                        if (requestLength >= -1) {
+                            json.data.splice(requestLength, json.data.length);
+                        }
+
+                        drawCallback(json);
+                    },
+                });
+            } else {
+                json = $.extend(true, {}, cacheLastJson);
+                json.draw = request.draw;
+                json.data.splice(0, requestStart - cacheLower);
+                json.data.splice(requestLength, json.data.length);
+
+                drawCallback(json);
+            }
+        };
+    };
+
+    function filterDataOmset() {
+        tableElem.DataTable({
+            "fixedColumns": {
+                start: 1,
+                end: 1
+            },
+            "scrollX": true,
+            "ordering": false,
+            "processing": true,
+            "serverSide": true,
+            "search": {
+                return: true,
+            },
+            "language": {
+                "processing": `<svg version="1.1" id="L4" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve" style="width: 250px;">
+                    <circle fill="#f26e23" stroke="none" cx="6" cy="50" r="6">
+                        <animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.1"></animate>    
+                    </circle>
+                    <circle fill="#f26e23" stroke="none" cx="26" cy="50" r="6">
+                        <animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.2"></animate>       
+                    </circle>
+                    <circle fill="#f26e23" stroke="none" cx="46" cy="50" r="6">
+                        <animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.3"></animate>     
+                    </circle>
+                </svg>
+                `,
+                "loadingRecords": "Loading...",
+                "emptyTable": "  "
+            },
+            "ajax": $.fn.dataTable.pipeline({
+                pages: 3,
+                url: "<?= url('master/all-omset-data') ?>",
+                crossDomain: false,
+                data: {
+                    '_token': $('meta[name="csrf-token"]').attr('content'),
+                    'regional': regional,
+                    'typeShopProduct': typeShopProduct,
+                    'shopProduct': shopProduct
+                },
+                method: 'POST',
+            }),
+            "infoCallback": function(settings, start, end, max, total, pre) {
+                return (!isNaN(total) && total > 0) ?
+                    "Showing " + start + " to " + end + " of " + total + " entries" +
+                    ((total !== max) ? " (filtered from " + max + " total entries)" : "") :
+                    "No Data to Show";
+            },
+            "columns": customColumn,
+        })
+    }
+
+    tableElem.on('processing.dt', function(e, settings, processing) {
+        var tableContainer = $('#datatable_wrapper');
+
+        if (processing) {
+            // Add class to prevent horizontal scrolling
+            tableContainer.addClass('prevent-horizontal-scroll');
+        } else {
+            // Remove class to allow horizontal scrolling
+            tableContainer.removeClass('prevent-horizontal-scroll');
+        }
     });
+
+    filterDataOmset()
+
+    $('#btn-filter-omset').click(function() {
+        regional = $('#SelectRegional').val()
+        shopProduct = $('#SelectShopKat').val()
+        typeShopProduct = $('#SelectShopKat').find(':selected').attr('data-tipeFilter')
+
+        tableElem.DataTable().destroy();
+        filterDataOmset()
+    })
 </script>
